d8cc047da129dfdd38e4cc6531d8df0a28c4d8fc
AbdelrahmanAbdelhamed <abdelrhmanabdelhamed@gmail.com>
2017-03-17 22:28:48 +0200

    added social groups

--- web-service/src/main/java/models/social/Post.java
+++ web-service/src/main/java/models/social/Post.java
@@ -3,18 +3,18 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package models.CMS;
+package models.social;
 
-import java.util.Date;
+import models.*;
 
 /**
  *
- * @author marcw
+ * @author AAGOOGLE
  */
-public class Assignment {
+public class Post extends SEntity {
 
-	String title, description;
-	Date dueDate, lateSubmits;
-	int maxGrade;
-	CourceOffering cource;
+	public User ownerUser;
+
+	public String title, content;
+
 }
