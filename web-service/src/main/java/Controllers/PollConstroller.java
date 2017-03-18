76ea48efff353e30265473a6751ae4f07e510f78
Marc Wafik <marc.wafik@hotmail.com>
2017-03-18 23:18:05 +0200

    added some controllers

---
+++ web-service/src/main/java/Controllers/PollConstroller.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controllers;
+
+/**
+ *
+ * @author marcw
+ */
+public class PollConstroller {
+
+	void submitAnswers() {
+
+	}
+
+	void createPoll() {
+
+	}
+
+	void updatePoll() {
+
+	}
+
+	void deletePoll() {
+
+	}
+}
