843b6014e5c29626cb117cfdb4ad74359ab9bd6e
Abdelrahman Abdelhamed <AbdelrhmanAbdelhamed@gmail.com>
2017-05-14 21:34:23 +0200

    - Crud

--- front-end/src/app/login/login.component.ts
+++ front-end/src/app/login/login.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, Params } from '@angular/router';
-import {ReactiveFormsModule, FormBuilder, FormGroup, Validators} from '@angular/forms';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 import { AuthenticationService } from '../authentication.service';
-
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-login',
@@ -14,22 +14,22 @@ export class LoginComponent implements OnInit {
 
   public signInForm: FormGroup;
 
-  constructor( private route: ActivatedRoute, private router: Router, private formBuilder: FormBuilder, private authenticationService: AuthenticationService) {
-   }
+  constructor(private route: ActivatedRoute, private router: Router, private formBuilder: FormBuilder, private authenticationService: AuthenticationService, private userService: UserService) {
+  }
 
   ngOnInit(): void {
     this.buildSignInForm();
   }
 
   buildSignInForm(): void {
-      this.signInForm = this.formBuilder.group({
-        email: ['', [Validators.required, Validators.pattern(/^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()\.,;\s@\"]+\.{0,1})+[^<>()\.,;:\s@\"]{2,})$/) ] ],
-        password: ['', [Validators.required, Validators.minLength(6)]]
+    this.signInForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.pattern(/^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()\.,;\s@\"]+\.{0,1})+[^<>()\.,;:\s@\"]{2,})$/)]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
     })
   }
 
   onSignInFormSubmit(): void {
-    if(this.authenticationService.signIn(this.signInForm.value)) {
+    if (this.authenticationService.signIn(this.signInForm.value)) {
     }
   }
 
