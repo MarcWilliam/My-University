80ff12175eee3bc216eda269f93c8bff08c6d207
Abdelrahman Abdelhamed <AbdelrhmanAbdelhamed@gmail.com>
2017-05-16 00:48:36 +0200

    - Edit user profile functionality

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { UserService } from './user.service';
+import { AuthenticationService } from './authentication.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  constructor() {
-   }
+export class AppComponent implements OnInit {
+  private title;
+  private user;
+
+  constructor(private authenticationService: AuthenticationService) {
+    this.title = 'My University';
+  }
+
+  ngOnInit() {
+    this.user = this.authenticationService.getCurrentUser();
+  }
 }
--- front-end/src/app/edit-profile/edit-profile.component.ts
+++ front-end/src/app/edit-profile/edit-profile.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { User } from '../user';
+import { UserService } from '../user.service';
+import { AuthenticationService } from '../authentication.service';
 
 @Component({
   selector: 'app-edit-profile',
@@ -7,15 +11,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditProfileComponent implements OnInit {
 
-    onSubmit(editProfile){
-    
+  private user: User;
 
-
+  constructor(private route: ActivatedRoute, private authenticationService: AuthenticationService, private userService: UserService) {
+    this.user = new User();
   }
 
-  constructor() { }
+  onSave() {
+    /*this.userService.update(this.user).subscribe(result => {
+      console.log(result);
+    });*/
+  }
 
   ngOnInit() {
-  }
 
+    this.route.queryParams.subscribe(params => {
+      const otherUserId = params['otherUserId'] || '0';
+
+      if (otherUserId !== '0') {
+        this.userService.read(otherUserId).subscribe(otherUser => {
+          this.user = <User>otherUser['data'][0];
+        });
+      } else {
+        this.user = <User>this.authenticationService.getCurrentUser();
+      }
+    });
+  }
 }
