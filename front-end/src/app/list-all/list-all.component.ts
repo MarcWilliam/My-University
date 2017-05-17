5399feb5cbb4db75e0146aac89d7fadfc984de0b
Abdelrahman Abdelhamed <AbdelrhmanAbdelhamed@gmail.com>
2017-05-17 16:09:06 +0200

    - Added Id selection (currentSelection$)

--- front-end/src/app/list-all/list-all.component.ts
+++ front-end/src/app/list-all/list-all.component.ts
@@ -1,8 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, OnDestroy, ViewChild } from '@angular/core';
 import { MdCheckboxModule } from '@angular/material';
 import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { CRUDService } from '../CRUD.service';
 import { ActivatedRoute, Params } from '@angular/router';
+
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/from';
+import { Subject } from 'rxjs/Subject';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+
+import {
+  MdDataTableComponent,
+  MdDataTablePaginationComponent,
+  IDatatableSelectionEvent,
+  IDatatableSortEvent,
+  IDatatablePaginationEvent,
+  DatatableSortType,
+} from 'ng2-md-datatable';
+
+import { CRUDService } from '../CRUD.service';
 import { ServiceFactory } from '../service-factory';
 
 @Component({
@@ -10,11 +25,15 @@ import { ServiceFactory } from '../service-factory';
   templateUrl: './list-all.component.html',
   styleUrls: ['./list-all.component.scss']
 })
-export class ListAllComponent implements OnInit {
+export class ListAllComponent implements OnInit, AfterViewInit, OnDestroy {
   public data: any;
   public headers: any;
 
   private crudService: CRUDService;
+  private currentSelection$: BehaviorSubject<string[]> = new BehaviorSubject([]);
+  private unmount$: Subject<void> = new Subject<void>();
+
+  @ViewChild(MdDataTableComponent) datatable: MdDataTableComponent;
 
   constructor(private route: ActivatedRoute, private serviceFactory: ServiceFactory) { }
 
@@ -22,9 +41,24 @@ export class ListAllComponent implements OnInit {
     this.route.queryParams.subscribe(params => {
       this.crudService = this.serviceFactory.getService(params['apiRoute']);
       this.crudService.read().subscribe(data => {
-        this.data = data
+        this.data = data;
         this.headers = Object.keys(this.data[0]);
       });
     });
   }
+
+  ngAfterViewInit() {
+    if (this.datatable) {
+      Observable.from(this.datatable.selectionChange)
+        .takeUntil(this.unmount$)
+        .subscribe((e: IDatatableSelectionEvent) => this.currentSelection$.next(e.selectedValues));
+    }
+  }
+
+  ngOnDestroy() {
+    this.unmount$.next();
+    this.unmount$.complete();
+  }
+
+
 }
