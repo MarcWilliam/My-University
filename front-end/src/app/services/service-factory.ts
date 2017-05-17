b6b5930bb7f293eec5c552373322c96f3e1c3af5
Abdelrahman Abdelhamed <AbdelrhmanAbdelhamed@gmail.com>
2017-05-17 20:54:07 +0200

    - Added course service to the service factory map

--- front-end/src/app/services/service-factory.ts
+++ front-end/src/app/services/service-factory.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@angular/core';
-import { CRUDService, UserService, UserRoleService } from '../services';
+import { CRUDService, UserService, UserRoleService, CourseService } from '../services';
 
 @Injectable()
 export class ServiceFactory {
   private routesMap: RoutesMap = {};
 
-  constructor(private userService: UserService, private userRoleService: UserRoleService) {
+  constructor(private userService: UserService, private userRoleService: UserRoleService, private courseService: CourseService) {
     this.routesMap[this.userService.apiRoute] = this.userService;
     this.routesMap[this.userRoleService.apiRoute] = this.userRoleService;
+    this.routesMap[this.courseService.apiRoute] = this.courseService;
   }
 
   getService(route: string) {
